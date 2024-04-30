ef80197f1711796db8981e9d8683571956ecfea9
VincentC27 <cardon.vincent@hotmail.fr>
2024-04-30 10:44:46 +0200

    add components folder

--- kasa-app-vite/src/App.jsx
+++ kasa-app-vite/src/App.jsx
@@ -1,9 +1,34 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { LogementsID } from './components/Logements/LogementsID';
+import { PageError } from './components/PageError/PageError';
+import { Home } from './components/Home/Home';
+import { About } from './components/About/About';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>,
+    errorElement: <PageError />,
+    children: [
+      {
+        path: 'logements/:id',
+        element: <LogementsID />
+      },
+
+    ],
+  },
+  { 
+    path: 'About',
+    element: <About />,
+    errorElement: <PageError />
+  }
+]);
+
+
+
 function App() {
-  return (
-   <div>
-    <p>Salut !</p>
-   </div>
-  )
+  return <RouterProvider router={router} />
 }
 
-export default App
+export default App;
---
+++ kasa-app-vite/src/component/Home/Home.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+import NavBarre from "../Header/Header";
+import Banner from "../Banner/Banner";
+import BannerHome from './banner-home.jpg'
+import Footer from "../Footer/Footer";
+import './Home.scss'
+
+export function Home() {
+
+  return (
+    <>
+      <NavBarre />
+      <Banner>
+        <div className="banner-container">
+          <img className="banner-container-img" src={BannerHome} alt='img acceuil' />
+          <p className="banner-container-txt">Chez vous, partout et ailleurs</p>
+        </div>
+      </Banner>
+      <Outlet />
+      <Footer /></>
+  )
+}
