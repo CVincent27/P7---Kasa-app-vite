1d6ee3906912d86ac4df8760b22b6df122720695
VincentC27 <cardon.vincent@hotmail.fr>
2024-05-04 21:16:06 +0200

    add Card scss

--- kasa-app-vite/src/components/Card/Card.jsx
+++ kasa-app-vite/src/components/Card/Card.jsx
@@ -4,12 +4,12 @@ import './Card.scss';
 
 const Card = ({ cardData }) => {
   return (
-    <div className="card-container">
-      <Link className="card" to={`logements/${cardData.id}`}>
+    <Link className="card" to={`logements/${cardData.id}`}>
+      <div className='card-wrap'>
         <h4 className='locationNom'>{cardData.title}</h4>
-          <img className='locationCover' src={cardData.cover} alt={cardData.title} />
-      </Link>
-    </div>
+        <img className='locationCover' src={cardData.cover} alt={cardData.title} />
+      </div>
+    </Link>
   );
 };
 
