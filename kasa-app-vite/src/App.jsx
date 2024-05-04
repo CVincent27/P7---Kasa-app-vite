2aa7662f0b9a2e5ee1425fa2d3f42f7447315fad
VincentC27 <cardon.vincent@hotmail.fr>
2024-05-04 20:26:05 +0200

    update router / path

--- kasa-app-vite/src/App.jsx
+++ kasa-app-vite/src/App.jsx
@@ -2,29 +2,26 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Error } from './pages/Error/Error';
 import { Home } from './pages/Home/Home';
 import { About } from './pages/About/About';
+import Logements from './pages/Logements/Logements';
 
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home/>,
-    errorElement: <Error />,
-    children: [
-      {
-        path: 'logements/:id',
-        // a modif
-      },
-
-    ],
+    errorElement: <Error />
   },
   { 
     path: 'About',
     element: <About />,
     errorElement: <Error />
+  },
+  {
+    path: 'logements/:id',
+    element: <Logements />,
+    errorElement: <Error />
   }
 ]);
-
-
 
 function App() {
   return <RouterProvider router={router} />
