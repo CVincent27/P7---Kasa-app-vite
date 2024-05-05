09cdc3c191f2f3fd4d8fead179c3463df4de9134
VincentC27 <cardon.vincent@hotmail.fr>
2024-05-05 13:46:22 +0200

    update logements.jsx

--- kasa-app-vite/src/pages/Logements/Logements.jsx
+++ kasa-app-vite/src/pages/Logements/Logements.jsx
@@ -1,9 +1,42 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import LogementData from '../../components/LogementData/LogementData';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 const Logements = () => {
+    // extraire id de l'URL
+    const { id } = useParams();
+    const [logement, setLogement] = useState(null);
+
+    //   recherche de données par id
+    const handleDataFetch = (data) => {
+        const selected = data.find(logement => logement.id === id);
+        setLogement(selected);
+    };
+
     return (
         <div>
-            <h1>Logements</h1>
+            <div className='main-content'>
+                <Header />
+                <LogementData onDataFetch={handleDataFetch} />
+                {logement && (
+                    <div key={logement.id}>
+                        <img src={logement.cover} alt={logement.title} />
+                        <h2>{logement.title}</h2>
+                        <p>{logement.description}</p>
+                        <p>
+                            {logement.host.name}
+                            <img src={logement.host.picture} alt={logement.host.name} />
+                        </p>
+                        <p>{logement.rating}</p>
+                        <p>{logement.location}</p>
+                    </div>
+                )}
+            </div>
+            <Footer />
         </div>
+
     );
 };
 
