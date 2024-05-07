5ffc6371805a83fb26e4ad16ee12f222ba013dc9
VincentC27 <cardon.vincent@hotmail.fr>
2024-05-07 13:07:48 +0200

    add slideshow component

---
+++ kasa-app-vite/src/components/Slideshow/Slideshow.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import ArrowRight from '../../assets/arrow-right.png';
+import ArrowLeft from '../../assets/arrow-left.png';
+import './Slideshow.scss';
+
+const Slideshow = ({ images }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    // voir pour modif (rassembler les 2 fonctions ?)
+    const nextSlide = () => {
+        if (currentIndex === images.length - 1) {
+            setCurrentIndex(0); 
+        } else {
+            setCurrentIndex(currentIndex + 1); 
+        }
+    };
+
+    const prevSlide = () => {
+        if (currentIndex === 0) {
+            setCurrentIndex(images.length - 1); 
+        } else {
+            setCurrentIndex(currentIndex - 1); 
+        }
+    };
+
+    return (
+        <section className='carousel'>
+            <div style={{ backgroundImage: `url(${images[currentIndex]})` }} className='carousel-content'>
+                {images.length > 1 && (
+                    <>
+                        <img
+                            className='carousel_arrow carousel_arrow_right'
+                            src={ArrowRight}
+                            alt='show next slider'
+                            onClick={nextSlide}
+                        />
+                        <img
+                            className='carousel_arrow carousel_arrow_left'
+                            src={ArrowLeft}
+                            alt='show previous slider'
+                            onClick={prevSlide}
+                        />
+                        <p className='slideCount'>{currentIndex + 1} / {images.length}</p>
+                    </>
+                )}
+            </div>
+        </section>
+    );
+};
+
+// Vzlidation props
+Slideshow.propTypes = {
+    images: PropTypes.arrayOf(PropTypes.string).isRequired 
+};
+
+export default Slideshow;
