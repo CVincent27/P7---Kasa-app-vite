fcd666e0be68cd029c24994945bca2b08c1ed7cc
VincentC27 <cardon.vincent@hotmail.fr>
2024-05-15 16:41:53 +0200

    add new Collapse componant
    test meilleure manière de faire la Collapse

--- kasa-app-vite/src/components/Collapse/Collapse.jsx
+++ kasa-app-vite/src/components/Collapse/Collapse.jsx
@@ -1,43 +1,80 @@
-import { useState } from 'react'
-import arrowUp from "../../assets/arrow-up.png"
-import arrowDown from "../../assets/arrow-down.png"
-import "./Collapse.scss"
+// import { useState } from 'react'
+// import arrowUp from "../../assets/arrow-up.png"
+// import arrowDown from "../../assets/arrow-down.png"
+// import "./Collapse.scss"
+// import PropTypes from 'prop-types'; 
+
+// function Collapse({title, content, contentClassName}) {
+//     // collapse fermée par defaut
+//     const [contentVisible, setContentVisible] = useState(false)
+
+//     // inverse valeur open/close
+//     const displayContent = () => { 
+//         setContentVisible(!contentVisible)
+//     }
+
+//     // def classe selon si c'est open ou close
+//     const collapseContent = (contentVisible ? "visible" : "hidden") + " collapse"
+//     const collapseArrow = (contentVisible ? arrowUp : arrowDown)
+
+//     return (
+//         <div className='collapse'>
+
+//             <div className='collapse-header' onClick={displayContent}>
+//                 <span>{title}</span>
+//                 <div className="arrowValue">
+//                     <img src={collapseArrow} alt=""/>
+//                 </div>
+//             </div>
+
+//             <div className={collapseContent} >
+//                 <ul className={contentClassName}>{content}</ul>
+//             </div>
+//         </div>
+//     )
+// }
+
+// // Déf proptypes
+// Collapse.propTypes = {
+//     title: PropTypes.string.isRequired, 
+//     content: PropTypes.node.isRequired,
+//     contentClassName: PropTypes.string
+// };
+
+// export default Collapse;
+
+import { useState } from 'react';
 import PropTypes from 'prop-types'; 
+import './Collapse.scss';
+import arrow from '../../assets/arrow.png';
 
-function Collapse({title, content}) {
-    // collapse fermée par defaut
-    const [contentVisible, setContentVisible] = useState(false)
+export default function Collapse({title, content}) {
 
-    // inverse valeur open/close
-    const displayContent = () => { 
-        setContentVisible(!contentVisible)
-    }
-
-    // def classe selon si c'est open ou close
-    const collapseContent = (contentVisible ? "visible" : "hidden") + " collapse"
-    const collapseArrow = (contentVisible ? arrowUp : arrowDown)
+    const [toggle, setToggle] = useState(false);
 
     return (
         <div className='collapse'>
-
-            <div className='collapse-header' onClick={displayContent}>
-                <span>{title}</span>
-                <div className="arrowValue">
-                    <img src={collapseArrow} alt=""/>
-                </div>
+            <p className='collapse-title' onClick={() => setToggle(!toggle)}>
+                {title}
+                <img 
+                    className={toggle ? 'arrow arrow_up' : 'arrow arrow_down'}
+                    src={arrow} 
+                    alt={title}
+                />
+            </p>
+            <div className={toggle ? 'collapse_content' : 'collapse_content_hidden'}>
+                    {Array.isArray(content) ? content.map((item, index) => {
+                        return (
+                            <p key={index}>{item}</p>
+                        )
+                    }) : content
+                    }
+                </div> 
             </div>
-
-            <div className={collapseContent}>
-                <ul>{content}</ul>
-            </div>
-        </div>
     )
-}
-
 // Déf proptypes
+}
 Collapse.propTypes = {
     title: PropTypes.string.isRequired, 
-    content: PropTypes.node.isRequired 
+    content: PropTypes.node.isRequired
 };
-
-export default Collapse;
