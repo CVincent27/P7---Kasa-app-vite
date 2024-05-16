cddc9c01ced8f72f615cc2df6237618a28e6f67e
VincentC27 <cardon.vincent@hotmail.fr>
2024-05-16 11:37:16 +0200

    add About page content

--- kasa-app-vite/src/pages/About/About.jsx
+++ kasa-app-vite/src/pages/About/About.jsx
@@ -3,6 +3,7 @@ import './About.scss'
 import Footer from "../../components/Footer/Footer"
 import Banner from "../../components/Banner/Banner";
 import BannerAbout from "../../assets/banner-about.jpg";
+import Collapse from "../../components/Collapse/Collapse";
 
 export function About() {
     return (
@@ -14,6 +15,28 @@ export function About() {
                         <img className="banner-container-img" src={BannerAbout} alt='img about' />
                     </div>
                 </Banner>
+                <div className="collapse-container">
+                    <Collapse
+                        title="Fiabilité"
+                        content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, 
+                        et toutes les informations sont régulièrement vérifiées par nos équipes."
+                    />
+                    <Collapse
+                        title="Respect"
+                        content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de 
+                        perturbation du voisinage entraînera une exclusion de notre plateforme."
+                    />
+                    <Collapse
+                        title="Service"
+                        content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de 
+                        perturbation du voisinage entraînera une exclusion de notre plateforme."
+                    />
+                    <Collapse
+                        title="Sécurité"
+                        content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de 
+                        perturbation du voisinage entraînera une exclusion de notre plateforme."
+                    />
+                </div>
             </div>
 
             <Footer />
